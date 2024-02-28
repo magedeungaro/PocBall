d8527eacdfc8d69ae4a12622de7c15ea3354b878
Mage Deungaro <81477400+magedeungaro@users.noreply.github.com>
2024-02-28 01:52:08 -0300

    still shitty code but hey it is working too

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const BOOST_THRESHOLD = {
+	down = 25,
+	up = 13
+}
+const CALCULATION_THRESHOLD = BOOST_THRESHOLD.up + 6
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -19,31 +24,44 @@ func distance_from_floor():
 		
 func is_going_down():
 	return velocity.y > 0
+	
+func is_going_up():
+	return not is_going_down()
 
-func is_boost_window():
-	const THRESHOLD = 25
-	var distance = distance_from_floor()
-	return distance and distance <= THRESHOLD
+func is_boost_window(distance):
+	if not distance: return false
+	if is_going_down():
+		return distance <= BOOST_THRESHOLD.down
+	else:
+		return distance <= BOOST_THRESHOLD.up
+	
+func is_calculation_window(distance):
+	if not distance: return false
+	return distance >= BOOST_THRESHOLD.up and distance <= CALCULATION_THRESHOLD and is_going_up()
 		
 func change_velocity(modifier):
 	velocity.y = JUMP_VELOCITY * modifier
 	
+func get_boost_modifier():
+	if additional_boost:
+		return 1.7
+	elif boost_jump:
+		return 1.3
+	
 func handle_jump():
-	# continuously jumping
+	var distance = distance_from_floor()
+	if is_going_up(): print(distance)
 	if is_on_floor(): change_velocity(1)
-	
-	if is_boost_window():
-		if Input.is_action_just_pressed("boost_jump"): boost_jump = true
-		if not is_going_down():
-			if boost_jump:
-				change_velocity(1.3)
-				boost_jump = false
-		
-	#if additional_boost < 0:
-		#additional_boost = 0
-	#elif additional_boost > 2:
-		#additional_boost = 1
-	
+	if is_boost_window(distance) and Input.is_action_just_pressed("boost_jump"):
+		boost_jump = true
+
+	if is_calculation_window(distance):
+		if boost_jump:
+			change_velocity(get_boost_modifier())
+			boost_jump = false
+			additional_boost = true
+		else:
+			additional_boost = false
 
 func _physics_process(delta):
 	handle_jump()
