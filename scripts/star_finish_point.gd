f53e603e1c1c079731e4edca12c1cd855503c58d
Mage Deungaro <81477400+magedeungaro@users.noreply.github.com>
2024-03-02 20:21:41 -0300

    getting score

--- scripts/star_finish_point.gd
+++ scripts/star_finish_point.gd
@@ -1,11 +1,24 @@
 extends Area2D
 
+@onready var count_up_timer = %CountUpTimer
 @export var target_level : PackedScene
 var leaderboard_key = null
 
+func _set_leaderboard_key():
+	var current_scene = get_tree().get_current_scene().get_name()
+	leaderboard_key = current_scene.to_lower()
+	
+func _change_scene():
+	get_tree().change_scene_to_packed(target_level)
+	
+func _send_player_info():
+	var data = { score=count_up_timer.time_elapsed,
+			 	 leaderboard_key = leaderboard_key }
+	print(data)
+
 func _on_body_entered(body):
 	if (body.name == "BallCharacter"):
-		var current_scene = get_tree().get_current_scene().get_name()
-		leaderboard_key = current_scene.to_lower()
-		print(leaderboard_key)
-		get_tree().change_scene_to_packed(target_level)
+		_set_leaderboard_key()
+		_send_player_info()
+		_change_scene()
+		
