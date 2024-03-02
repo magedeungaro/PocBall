bbd57c48a4eea024ecf4877bf463a8c2c35e5163
Mage Deungaro <81477400+magedeungaro@users.noreply.github.com>
2024-03-02 19:56:50 -0300

    get leaderboard key when level is finished

--- scripts/star_finish_point.gd
+++ scripts/star_finish_point.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
 @export var target_level : PackedScene
+var leaderboard_key = null
 
 func _on_body_entered(body):
 	if (body.name == "BallCharacter"):
+		var current_scene = get_tree().get_current_scene().get_name()
+		leaderboard_key = current_scene.to_lower()
+		print(leaderboard_key)
 		get_tree().change_scene_to_packed(target_level)
-
-func _on_process(_delta):
-	print(target_level)
