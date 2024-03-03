cc5287b620e9abb5ff67dbf2abc2faab38e5f6a7
johngvc <johngvc@gmail.com>
2024-03-02 23:00:03 -0300

    Implemented Rising water feature

---
+++ scripts/rising_water.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+const height_to_rise = 952
+const water_origin = Vector2(827, 1576)
+const water_retreat_speed = 0.05
+const water_rising_speed = 0.01
+
+var distance_traveled_normalized = 0.0
+var start_position = water_origin
+var end_position = Vector2(water_origin.x, water_origin.y - height_to_rise)
+var delay_water_for = 0
+
+
+func _on_body_entered(body):
+	if (body.name == "BallCharacter"):
+		print("player lost")
+
+func _physics_process(delta):
+	
+	if delay_water_for > 0:
+		distance_traveled_normalized -= delta * water_retreat_speed
+		delay_water_for -= delta
+		delay_water_for = max(delay_water_for, 0)
+	else:
+		distance_traveled_normalized += delta * water_rising_speed
+
+	if distance_traveled_normalized <= 1:
+		position = start_position.lerp(end_position, distance_traveled_normalized)
