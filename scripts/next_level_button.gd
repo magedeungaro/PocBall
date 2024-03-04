b61188817f7693c336796c8a354ef1da2318d7ba
Mage Deungaro <81477400+magedeungaro@users.noreply.github.com>
2024-03-03 23:00:38 -0300

    handle last level leaderboard

--- scripts/next_level_button.gd
+++ scripts/next_level_button.gd
@@ -2,10 +2,14 @@ extends Button
 
 @onready var game_manager = GameManager
 var next_level = null
+var next_level_scene = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	next_level = game_manager.get_next_level()
 	if not next_level: queue_free()
+	
+	next_level_scene = "res://scenes/levels/"+next_level+".tscn"
+	if not ResourceLoader.exists(next_level_scene): queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,4 +18,4 @@ func _process(_delta):
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://scenes/levels/"+next_level+".tscn")
+	get_tree().change_scene_to_file(next_level_scene)
